df998d61cb128c0f68b7d1194fa6be5a3dbd8862
phitranpt <phi.tran.pt@gmail.com>
2019-11-15 23:43:52 -0600

    correct jquery script

--- scripts/client.js
+++ scripts/client.js
@@ -1,21 +1,8 @@
-console.log('js works')
-
-const oneProg = 165;
-const twoProg = 185;
-const kidsProg = 130;
-
-function addOneProg () {
-    console.log('adding one program', oneProg * .4);
-}
-
-function addTwoProg () {
-    return twoProg * .4;
-}
-
-function addAdditionalKids () {
-    return kidsProg * .4
-}
-
 $(document).ready(function () {
-    //your code here
-  });
+    $( '#calculate' ).on( 'click', function() {
+        let oneProgram = $('#oneProgram').val();
+        let twoProgram = $('#twoProgram').val();
+        let kidsProgram = $('#kidsProgram').val();
+        console.log('one', parseInt(oneProgram));
+    });
+}); //end doc ready
