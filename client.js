80fc5d1ba20fe8b4e3222a8abada0bf37092b765
phitranpt <phi.tran.pt@gmail.com>
2019-11-15 22:47:34 -0600

    added functions to calc discount

--- client.js
+++ client.js
@@ -1,4 +1,24 @@
 console.log('js works')
 
+const oneProg = 165;
+const twoProg = 185;
+const kidsProg = 130;
+
+function addOneProg () {
+    return oneProg * .4;
+}
+
+function addTwoProg () {
+    return twoProg * .4;
+}
+
+function addAdditionalKids () {
+    return kidsProg * .4
+}
+
+console.log('adding additional person w/ one program', addOneProg())
+console.log('adding additional person w/ discount', addTwoProg());
+console.log('adding additional kids w/ discount', addAdditionalKids());
+
 
 
