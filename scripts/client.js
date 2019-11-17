1aef574bafead2ae659bfea51cf5eb29387b3819
phitranpt <phi.tran.pt@gmail.com>
2019-11-17 11:05:22 -0600

    added new table and coded client with adding new member

--- scripts/client.js
+++ scripts/client.js
@@ -1,8 +1,24 @@
+let memberCount = 1;
+
 $(document).ready(function () {
-    $( '#calculate' ).on( 'click', function() {
-        let oneProgram = $('#oneProgram').val();
-        let twoProgram = $('#twoProgram').val();
-        let kidsProgram = $('#kidsProgram').val();
-        console.log('one', parseInt(oneProgram));
+
+    $( '#addMemberButton' ).on( 'click', function() {
+        let newMember = `<tr><td>${memberCount++}</td>`;
+        let deleteButton = `<td><button id="deleteButton">Delete</button></td></tr>`;
+        let newRow = newMember + deleteButton;
+        $('table tbody').append(newRow);
     });
+
+    $( '#deleteButton' ).on( 'click', function() {
+        console.log('delete button works');
+    });
+
+
+
+
+
+
+
+
+
 }); //end doc ready
