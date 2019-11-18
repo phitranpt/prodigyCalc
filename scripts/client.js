3936ac5e148c0e84af66bed53e9da13abc820dfc
phitranpt <phi.tran.pt@gmail.com>
2019-11-17 18:30:34 -0600

    installed bootstrap and added a few new columns in table

--- scripts/client.js
+++ scripts/client.js
@@ -1,14 +1,23 @@
+//setting variables
 let memberCount = 1;
+let oneAdultProgram = 165;
+let twoAdultProgram = 185
+let kidsProgram = 130;
 
 $(document).ready(function () {
 
+    //add new members
     $( '#addMemberButton' ).on( 'click', function() {
         let newMember = `<tr><td>${memberCount++}</td>`;
+        let memberLevel = `<td>'Member Level'</td>`;
+        let extraDiscount = `<td>'Extra Discount'</td>`;
+        let memberPrice = `<td>'Member Price'</td>`;
         let deleteButton = `<td><button id="deleteButton">Delete</button></td></tr>`;
-        let newRow = newMember + deleteButton;
+        let newRow = newMember + memberLevel + extraDiscount + memberPrice + deleteButton;
         $('table tbody').append(newRow);
     });
 
+    //delete members
     $( '#myTable' ).on( 'click', '#deleteButton', function() {
         console.log('delete button works');
         $(this).closest('tr').remove();
